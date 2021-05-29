230ae4872808e4d3e5cbe518800ba8ab2888303d
zjghb <qianvzj@163.com>
2021-05-29 19:37:20 +0800

    第四次提交

--- demo/demo-java/src/main/java/com/example/demojava/Demo.java
+++ demo/demo-java/src/main/java/com/example/demojava/Demo.java
@@ -1,8 +1,13 @@
 package com.example.demojava;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class Demo {
 
     public static void main(String[] args) {
         System.out.println("helloworld");
+        new ThreadPoolExecutor(1, 5, 10, TimeUnit.SECONDS,new ArrayBlockingQueue<>(5));
     }
 }
